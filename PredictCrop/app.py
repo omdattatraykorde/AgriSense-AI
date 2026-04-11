28b14c5505eebb3868208adc058415f92b354848
Om7470 <kordeom445@gmail.com>
2026-04-11 20:12:34 +0530

    Update app.py

--- PredictCrop/app.py
+++ PredictCrop/app.py
@@ -36,6 +36,7 @@ def predict():
 
     return jsonify({"recommended_crop": crop})
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
